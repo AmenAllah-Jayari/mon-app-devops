750b5a55a29067e2484e1fae0db1708f5315518a
amen <amenallahjayari04@gmail.com>
2026-05-18 15:13:17 +0200

    Version 2.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Hello DevOps ! Version 2.0</h1>');
+  res.end('<h1>Hello DevOps ! Version 3.0</h1>');
 });
 
 server.listen(port, () => {
